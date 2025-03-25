64be89589b1277d6fcb29d09eab2d9984a97e81b
testaccount911 <bassettia@students.dsbn.org>
2025-03-25 12:42:20 -0400

    okie doki

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+# Name: Ian Bassett-Kollee
+# Title: 
+# Date: 
+# Description: 
 
+import random 
+
+numbers =[]
+for i in range (100):
+    num=random.randint(1,100)
+    numbers.append(num)
+
+print (sum (numbers)/100) 
